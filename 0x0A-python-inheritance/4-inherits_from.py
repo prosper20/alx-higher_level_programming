13658b8d1a37cd95987c2f9b5cea4186cc1de303
Bobson Prosper Paebi <bobsonconnect@gmail.com>
2023-06-12 19:29:59 -0700

    Updated

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -20,6 +20,6 @@ def inherits_from(obj, a_class):
         True if obj is an instance of a_class
         False, otherwise
     """
-    if issubclass(type(obj), a_class) and type(obj) != a_class:
-        return True
-    return False
+    if type(obj) is a_class:
+        return False
+    return True
