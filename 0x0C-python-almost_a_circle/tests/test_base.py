f762dc596aaee4ec5efcc155fa0c8fb6a560e724
prosper20 <bobsonconnect@gmail.com>
2023-06-19 12:44:21 -0700

    Added test for base

---
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" Module for test Base class """
+import unittest
+from models.base import Base
+from models.square import Square
+from models.rectangle import Rectangle
+from io import StringIO
+from unittest import TestCase
+from unittest.mock import patch
+
+
+class TestBaseMethods(unittest.TestCase):
+    """ Suite to test Base class """
+
+    def setUp(self):
+        """ Method invoked for each test """
+        Base._Base__nb_objects = 0
+
+    def test_id(self):
+        """ Test assigned id """
+        new = Base(1)
+        self.assertEqual(new.id, 1)
+
+    def test_id_default(self):
+        """ Test default id """
+        new = Base()
+        self.assertEqual(new.id, 1)
+
+    def test_id_nb_objects(self):
+        """ Test nb object attribute """
+        new = Base()
+        new2 = Base()
+        new3 = Base()
+        self.assertEqual(new.id, 1)
+        self.assertEqual(new2.id, 2)
+        self.assertEqual(new3.id, 3)
